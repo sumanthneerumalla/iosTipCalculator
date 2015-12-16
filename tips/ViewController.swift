988c8be0f25b851abca727287b5ba06e8e554546
Sumanth <sumanth@sanjanas-macbook-pro.local>
2015-12-16 00:44:02 -0500

    Built first version of tip calculating app. Calculates tip amount and updates total as user modifies bill amount

--- tips/ViewController.swift
+++ tips/ViewController.swift
@@ -10,9 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        tipLabel.text = "$0.00"
+        totalLabel.text = "$0.00"
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +27,16 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onEditingChanged(sender: AnyObject) {
+        
+        var billAmount = NSString(string:  billField.text!).doubleValue
+        var tipAmount = billAmount * 0.2
+        var total = billAmount + tipAmount
+        
+        tipLabel.text = "$\(tipAmount)"
+        totalLabel.text = "$\(total)"
+        
+    }
 
 }
 
